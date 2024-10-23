a87f3762c54e5b5a96a37a2579f4cff01a90af64
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-24 06:23:58 +0800

    Add eslint stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -27,6 +28,7 @@ export default tseslint.config(
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic': stylistic,
     },
     rules: {
       ...react.configs.recommended.rules,
@@ -36,6 +38,17 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...stylistic.configs['recommended-flat'].rules,
+
+      // my preferences
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/multiline-ternary': 'off',
+      '@stylistic/quotes': [
+        'error',
+        'single',
+        { allowTemplateLiterals: true, avoidEscape: true },
+      ],
+      '@stylistic/jsx-one-expression-per-line': 'off',
     },
   },
 )
