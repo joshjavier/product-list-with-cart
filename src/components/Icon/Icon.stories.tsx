5a9d60c8fc0e63ea2e135023bad8b28b8ebc62b3
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-27 08:14:42 +0800

    Refactor Icon component

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,10 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import Icon from './Icon'
-import iconPlus from '../../icons/plus.svg'
-import iconMinus from '../../icons/minus.svg'
-import iconX from '../../icons/x.svg'
-import iconCart from '../../icons/shopping-cart-plus.svg'
 
 const meta: Meta<typeof Icon> = {
   component: Icon,
@@ -15,24 +11,24 @@ type Story = StoryObj<typeof Icon>
 
 export const Plus: Story = {
   args: {
-    icon: iconPlus,
+    icon: 'add',
   },
 }
 
 export const Minus: Story = {
   args: {
-    icon: iconMinus,
+    icon: 'subtract',
   },
 }
 
 export const Cancel: Story = {
   args: {
-    icon: iconX,
+    icon: 'remove',
   },
 }
 
 export const Cart: Story = {
   args: {
-    icon: iconCart,
+    icon: 'cart',
   },
 }
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,9 +1,34 @@
+import iconPlus from '../../icons/plus.svg'
+import iconMinus from '../../icons/minus.svg'
+import iconX from '../../icons/x.svg'
+import iconCart from '../../icons/shopping-cart-plus.svg'
+
 export interface Props {
-  icon: string
+  icon: IconVariant
   alt?: string
   size?: number
 }
 
+export type IconVariant = 'add' | 'subtract' | 'cart' | 'remove'
+
+const getIconUrl = (icon: IconVariant): string => {
+  return {
+    subtract: iconMinus,
+    add: iconPlus,
+    cart: iconCart,
+    remove: iconX,
+  }[icon]
+}
+
 export default function Icon({ icon, alt = '', size = 20 }: Props) {
-  return <img className="icon" src={icon} alt={alt} width={size} height={size} />
+  const iconUrl = getIconUrl(icon)
+  return (
+    <img
+      className="icon"
+      src={iconUrl}
+      alt={alt}
+      width={size}
+      height={size}
+    />
+  )
 }
